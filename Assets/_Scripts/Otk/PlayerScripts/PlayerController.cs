c86804963bf5dd5f2954d57e3e3047b8a735e5dd
James Carl <jamescarljunio@gmail.com>
2022-09-18 00:51:46 +0800

    added player instantiation

--- Assets/_Scripts/Otk/PlayerScripts/PlayerController.cs
+++ Assets/_Scripts/Otk/PlayerScripts/PlayerController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    Vector2 movement;
+    PhotonView PV;
+
+    void Awake() {
+        PV = GetComponent<PhotonView>();
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        if (!PV.IsMine) {
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+            Destroy(GetComponentInChildren<Cinemachine.CinemachineVirtualCamera>().gameObject);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (!PV.IsMine) return;
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate() {
+        if (!PV.IsMine) return;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
